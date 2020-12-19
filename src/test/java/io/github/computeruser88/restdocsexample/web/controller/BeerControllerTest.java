10ffafb7ecaef1f06fec90464c30bbdfb668ebe7
Jan Balangue <jan.a.balangue@gmail.com>
2020-12-18 20:15:11 -0600

    Add documentation

--- src/test/java/io/github/computeruser88/restdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/io/github/computeruser88/restdocsexample/web/controller/BeerControllerTest.java
@@ -1,21 +1,40 @@
 package io.github.computeruser88.restdocsexample.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.computeruser88.restdocsexample.domain.Beer;
+import io.github.computeruser88.restdocsexample.repositories.BeerRepository;
 import io.github.computeruser88.restdocsexample.web.model.BeerDto;
 import io.github.computeruser88.restdocsexample.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+@WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "io.github.computeruser88.restdocsexample.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -24,11 +43,34 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                .param("iscold", "yes")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/beer", pathParameters(
+                        parameterWithName("beerId").description("UUID of desired beer to get.")
+                        ),
+                        requestParameters(
+                                parameterWithName("iscold").description("Is Beer Cold Query param")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("id of Beer"),
+                                fieldWithPath("version").description("Version number"),
+                                fieldWithPath("createdDate").description("Date Created"),
+                                fieldWithPath("lastModifiedDate").description("Date Updated"),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of Beer"),
+                                fieldWithPath("price").description("Price"),
+                                fieldWithPath("quantityOnHand").description("Quantity On Hand")
+                        )));
     }
 
     @Test
@@ -58,10 +100,10 @@ class BeerControllerTest {
 
     BeerDto getValidBeerDto() {
         return BeerDto.builder()
-                .beerName("My Beer")
+                .beerName("Nice Ale")
                 .beerStyle(BeerStyleEnum.ALE)
-                .price(new BigDecimal("2.99"))
-                .upc(1234328834572L)
+                .price(new BigDecimal("9.99"))
+                .upc(123432835463472L)
                 .build();
     }
 }
